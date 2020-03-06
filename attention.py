Fixed the attention calculation, still need minor modification to use on #groups > 1

attention.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
-
+import numpy
 
 class AttentionConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, groups=1, bias=False):
@@ -21,7 +21,8 @@ class AttentionConv(nn.Module):
         assert self.out_channels % self.groups == 0, "out_channels should be divided by groups. (example: out_channels: 40, groups: 4)"
 
         # Note the different usage of kernel_size for rel_w and rel_h. They are 2 one dimensional arrays
-        # TODO : Remove the division by 2 in the channels
+        # Reason they divide by two is that in the paper they just concat rel_h and rel_w to be the positional
+        # embedding vector
         self.rel_h = nn.Parameter(torch.randn(out_channels // 2, 1, 1, kernel_size, 1), requires_grad=True)
         self.rel_w = nn.Parameter(torch.randn(out_channels // 2, 1, 1, 1, kernel_size), requires_grad=True)
 
@@ -44,21 +45,32 @@ class AttentionConv(nn.Module):
 
         k_out = k_out.unfold(2, self.kernel_size, self.stride).unfold(3, self.kernel_size, self.stride)
         v_out = v_out.unfold(2, self.kernel_size, self.stride).unfold(3, self.kernel_size, self.stride)
-        ###########Until here it was all good. We have doubts starting from this point.
+        #now k_out has shape (bsz, out_channels, height, width,3,3) where kernel =(3,3) (so has keys for each block which makes it easy to apply attention)
 
+        #now we add relative height to the first half of the output channels and relative width to the second half
         k_out_h, k_out_w = k_out.split(self.out_channels // 2, dim=1)
         k_out = torch.cat((k_out_h + self.rel_h, k_out_w + self.rel_w), dim=1)
 
+        #for now suppose groups is 1, RETHINK THIS IF NOT
+        #this operation just flattens the kernels in the last two dimensions (does this properly from example I did)
         k_out = k_out.contiguous().view(batch, self.groups, self.out_channels // self.groups, height, width, -1)
         v_out = v_out.contiguous().view(batch, self.groups, self.out_channels // self.groups, height, width, -1)
 
         q_out = q_out.view(batch, self.groups, self.out_channels // self.groups, height, width, 1)
 
-        out = q_out * k_out
-        out = F.softmax(out, dim=-1)
-        out = torch.einsum('bnchwk,bnchwk -> bnchw', out, v_out).view(batch, -1, height, width)
+        #DONT UNDERSTAND WHY HE MULTIPLIED LIKE THIS, HIS IS COMMENTED OUT
+        #out = q_out * k_out
+        #out = F.softmax(out, dim=-1)
+        #out = torch.einsum('bnchwk,bnchwk -> bnchw', out, v_out).view(batch, -1, height, width)
 
-        return out
+        #I think way to do this is is multiply (broadcast over last dimension) then sum dim=2 (acts as dot product)
+        #TO DO: Check that this still works with groups > 1 (I think may need to do a flattening after in this case
+        out = (q_out*k_out).sum(dim=2)
+        out2 = F.softmax(out, dim=-1)
+        out3 = (out2.unsqueeze(dim=2) * v_out).sum(dim=-1).squeeze(dim=1) #Check if can condense this in one einstein
+
+        return out3
+
 
     def reset_parameters(self):
         init.kaiming_normal_(self.key_conv.weight, mode='fan_out', nonlinearity='relu')
@@ -69,6 +81,7 @@ class AttentionConv(nn.Module):
         init.normal_(self.rel_w, 0, 1)
 
 
-temp = torch.randn((2, 3, 32, 32))
-conv = AttentionConv(3, 16, kernel_size=3, padding=1)
-print(conv(temp).size())
+if __name__ == '__main__':
+    temp = torch.randn((2, 3, 32, 32))
+    conv = AttentionConv(3, 16, kernel_size=3, padding=1)
+    print(conv(temp).size())