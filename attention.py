f7c2a560c6b2a41c6f2b3dd25a75b9ba3ad6fcc1
Shakti Kumar <shakti.shrivastava13@gmail.com>
2020-04-09 12:29:37 -0400

    adaptive verified

--- attention.py
+++ attention.py
@@ -10,7 +10,6 @@ import time #TODO... Remove, only for testing purposes
 This class is taken from https://github.com/facebookresearch/adaptive-span/blob/master/adaptive_span.py
 but has been adapted to work with 2d inputs
 '''
-# TODO: Get this working with multiple heads
 class AdaptiveMask(nn.Module):
     """Soft masking function for adaptive size.
     It masks out the last K values of an input. The masking value
@@ -33,15 +32,15 @@ class AdaptiveMask(nn.Module):
         self.mask_template = torch.linspace(1 - int(max_size), 0, steps=int(max_size))
 
     def forward(self, x, mask_len):
-        # TODO : Why should you use a mask_len at all?
 
+        # self.current_val should be a fraction
         one_d_mask = self.mask_template + self.current_val * self._max_size
         one_d_mask = one_d_mask / self._ramp_size + 1
         one_d_mask = one_d_mask.clamp(0, 1)
         # TODO Debug: Check that indexing right dim, this should be relative to x size
         #              if kernel is 3x3 then would expect x.shape[-1] to be 3
 
-        # TODO: check what is the mask_len here? one_d_mask is max_size itself right? you dont need to make changes here
+        # This line is to do the computation only for the mask_len size which are non zeros. Avoiding some compute here
         one_d_mask = one_d_mask[:,-mask_len:]
         kernel_size = 2*mask_len+1
         # Now masking 'out' (how do we count distance? One way is to start at the center pixel of the kernel and
@@ -58,24 +57,16 @@ class AdaptiveMask(nn.Module):
             indices += [[top, j] for j in range(left + 1, right + 1)]  # top minus overlap with left
             indices += [[j, right] for j in range(bottom + 1, top)]  # right minus overlap with bottom and top
             rows, cols = zip(*indices)
-
-            # TODO : Doubt why is this 0 here? what is the first dimension of one_d_mask if the number of heads is 1, which is in our case here
-            mask[rows, cols] = one_d_mask[0,i] # TODO: Check how should be indexing this when multiple groups
+            mask[rows, cols] = one_d_mask[0,i]
 
             left += 1
             right -= 1
 
+        # this trimming is already done in line 44
         #if x.size(-1) < self._max_size:
         #    # the input could have been trimmed beforehand to save computation
         #    mask = mask[:, :, -x.size(-1):]
 
-        #TODO: make sure that when I flatten mask, that it is reshaped in exactly the
-        #     same way that x was when it has it's final two dimensions flattened.
-        #     THIS IS WHERE THINGS COULD GO WRONG IF X IS FLATTENED DIFFERENTLY THAN
-        #      mask WAS.
-
-        # TODO : Why should you flatten it at all? the x is the attention, now shouldn't the attention shape also
-        #       be in the original dimension of the layer you're currently working on?
         mask = mask.view(1,1,1,1,-1)
         x = x * mask
         x = x / (x.sum(-1, keepdim=True) + 1e-8)
@@ -106,7 +97,7 @@ class AdaptiveMask(nn.Module):
 
 class AttentionConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, groups=1, bias=False,
-                 R=3, z_init=4, image_size=32, adaptive_span=False):
+                 R=3, z_init=0.3, image_size=32, adaptive_span=False):
 
         super(AttentionConv, self).__init__()
         self.out_channels = out_channels
@@ -126,7 +117,6 @@ class AttentionConv(nn.Module):
 
 
         max_mask_size = image_size / 2 # TODO(Joe): Our images are all even sizes now so this works but we should force this to be an int, i.e. int(image_size / 2) or image_size // 2
-        # TODO : Why divide by 2 here?
 
         self.adaptive_mask = AdaptiveMask(max_mask_size, R, init_val=z_init,
                                           shape=(groups, 1))
@@ -203,7 +193,6 @@ class AttentionConv(nn.Module):
             rel_w = self.rel_w
 
         k_out_h, k_out_w = k_out.split(self.out_channels // 2, dim=1)
-        # TODO : This relative positioning is not clear !
         k_out = torch.cat((k_out_h + rel_h, k_out_w + rel_w), dim=1)
 
 
@@ -214,7 +203,6 @@ class AttentionConv(nn.Module):
         k_out = k_out.contiguous().view(batch, self.groups, self.out_channels // self.groups, height, width, -1)
         v_out = v_out.contiguous().view(batch, self.groups, self.out_channels // self.groups, height, width, -1)
 
-        # TODO : How will this expansion work out if there are multiple groups ?
         q_out = q_out.view(batch, self.groups, self.out_channels // self.groups, height, width, 1)
 
         # ORINGINAL IMPLEMENTATION
@@ -233,7 +221,6 @@ class AttentionConv(nn.Module):
         start_time = time.time()
         out = (q_out*k_out).sum(dim=2) # Original
         # All the channels are being merged into 1
-        # TODO Shakti : The last sum is weird!!
         #out = (q_out*k_out).sum(dim=2).squeeze(dim=1)
 
         out2 = F.softmax(out, dim=-1)
@@ -260,5 +247,5 @@ class AttentionConv(nn.Module):
 
 if __name__ == '__main__':
     temp = torch.randn((2, 3, 32, 32))
-    conv = AttentionConv(3, 16, kernel_size=3, padding=1)
+    conv = AttentionConv(3, 16, kernel_size=3, padding=1, adaptive_span=True)
     print(conv(temp).size())
